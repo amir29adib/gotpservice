580b6b606b71e9586e2b9d1c25f57f0c781ca3f4
amir <amir29adib1234@gmail.com>
2025-08-30 02:05:18 +0330

    refactor jwt utils in generate and parse it

--- pkg/utils/jwt.utils.go
+++ pkg/utils/jwt.utils.go
@@ -2,20 +2,41 @@ package utils
 
 import (
 	"os"
-    "time"
-    "github.com/golang-jwt/jwt/v5"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
+type Claims struct {
+    UserID uint   `json:"user_id"`
+    Phone  string `json:"phone"`
+    jwt.RegisteredClaims
+}
+
 func GenerateJWT(userID uint, phone string) (string, error) {
-    claims := jwt.MapClaims{
-        "user_id": userID,
-        "phone":   phone,
-        "exp":     time.Now().Add(24 * time.Hour).Unix(), // 24h expiry
-        "iat":     time.Now().Unix(),
+    claims := Claims{
+        UserID: userID,
+        Phone:  phone,
+        RegisteredClaims: jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+            IssuedAt:  jwt.NewNumericDate(time.Now()),
+        },
     }
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
     return token.SignedString(jwtSecret)
+}
+
+func ParseJWT(tokenString string) (*Claims, error) {
+    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        return []byte("secret-key"), nil
+    })
+
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+        return claims, nil
+    } else {
+        return nil, err
+    }
 }
